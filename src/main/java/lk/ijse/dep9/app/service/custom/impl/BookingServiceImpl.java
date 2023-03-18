b7008bb084de6a1c077c885433d37827eb3d78a7
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-18 11:06:22 +0530

    Complete BookingServiceImpl logic

--- src/main/java/lk/ijse/dep9/app/service/custom/impl/BookingServiceImpl.java
+++ src/main/java/lk/ijse/dep9/app/service/custom/impl/BookingServiceImpl.java
@@ -1,10 +1,15 @@
 package lk.ijse.dep9.app.service.custom.impl;
 
 import lk.ijse.dep9.app.dto.BookingDTO;
+import lk.ijse.dep9.app.dto.PassengerInfoDTO;
+import lk.ijse.dep9.app.dto.PaymentInfoDTO;
+import lk.ijse.dep9.app.entity.PassengerInfo;
+import lk.ijse.dep9.app.entity.PaymentInfo;
 import lk.ijse.dep9.app.repository.PassengerInfoRepository;
 import lk.ijse.dep9.app.repository.PaymentInfoRepository;
 import lk.ijse.dep9.app.service.custom.BookingService;
 import lk.ijse.dep9.app.service.util.Transformer;
+import lk.ijse.dep9.app.util.PaymentUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -29,6 +34,19 @@ public class BookingServiceImpl implements BookingService {
     @Override
     @Transactional(readOnly = false, rollbackFor = Throwable.class, propagation = Propagation.REQUIRED)
     public void createNewBooking(BookingDTO bookingDTO) {
+        PassengerInfoDTO passengerInfo = bookingDTO.getPassengerInfoDTO();
+        PassengerInfo savedPassenger = passengerInfoRepository.save(transformer.toPassengerInfoEntity(passengerInfo));
 
+        PaymentInfoDTO paymentInfoDTO = bookingDTO.getPaymentInfoDTO();
+
+        /**
+         * Check the available amount in the account.
+         * If request fare is exceed the available amount, Data will not save to both PassengerInfo and PaymentInfo tables, transaction will be rollback.
+         */
+        PaymentUtils.validateCreditLimit(paymentInfoDTO.getAccountNo(), savedPassenger.getFare());
+
+        PaymentInfo paymentInfo = transformer.toPaymentInfoEntity(paymentInfoDTO);
+        paymentInfo.setPassengerInfo(savedPassenger);
+        paymentInfoRepository.save(paymentInfo);
     }
 }
