de17a7b926a1b48b4834e998aadc58867dae6c73
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-18 10:36:32 +0530

    Create PaymentInfo Repo

---
+++ src/main/java/lk/ijse/dep9/app/repository/PaymentInfoRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep9.app.repository;
+
+import lk.ijse.dep9.app.entity.PaymentInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PaymentInfoRepository extends CrudRepository<PaymentInfo, Integer> {
+}
