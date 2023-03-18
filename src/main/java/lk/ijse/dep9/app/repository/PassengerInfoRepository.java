02b5b0658bba8c3859639bda25f12d7c33be9c3f
Pubudu Janith <pubudujanith123@gmail.com>
2023-03-18 10:35:24 +0530

    Create PassengerInfoRepo

---
+++ src/main/java/lk/ijse/dep9/app/repository/PassengerInfoRepository.java
@@ -0,0 +1,7 @@
+package lk.ijse.dep9.app.repository;
+
+import lk.ijse.dep9.app.entity.PassengerInfo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PassengerInfoRepository extends CrudRepository<PassengerInfo, Integer> {
+}
